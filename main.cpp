f5d62eb55fcebb0ae2dc40cefdda11a36e8cba43
Paul Ajibola <ife.jibola@gmail.com>
2021-05-07 01:25:12 -0400

    hello world

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream> // instructs the complier to include the declaration of the standard stream I/O facilities as found in iostream.
+
+int main()
+{
+    std::cout << "Hello world\n";
+}
